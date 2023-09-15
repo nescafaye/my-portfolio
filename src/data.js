650e47916b18c6190acd85e8535320d0257089a0
faye <talayfaye@gmail.com>
2023-09-16 01:46:46 +0800

    data

---
+++ src/data.js
@@ -0,0 +1,141 @@
+export const data = {
+    name: "Faye Talay",
+    intro: "I'm into Web, UI, and Design.",
+    desc: "An aspiring developer and designer based at Cagayan Valley, Philippines.",
+    link: "drive.pdf",
+    socials: [
+        {
+            socialName: "LinkedIn",
+            socialUrl: "https://linkedin.com/in/faye-talay"
+        },
+        {
+            socialName: "Github",
+            socialUrl: "https://github.com/nescafaye"
+        },
+        {
+            socialName: "Behance",
+            socialUrl: "https://behance.com/nescafaye"
+        },
+        {
+            socialName: "Dribble",
+            socialUrl: "https://dribbble.com/nescafaye"
+        }   
+    ],
+    field: [
+        {
+            fieldName: "Web Development",
+            stack: [
+                {
+                    stackName: "HTML",
+                    level:"advanced"
+                },
+                {
+                    stackName: "CSS",
+                    level:"advanced"
+                },
+                {
+                    stackName: "Javascript",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "React",
+                    level:"beginner"
+                },
+                {
+                    stackName: "Angular",
+                    level:"beginner"
+                },
+                {
+                    stackName: "Vue",
+                    level:"beginner"
+                },
+                {
+                    stackName: "Laravel",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "Tailwind",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "Bootstrap",
+                    level:"intermediate"
+                },
+            ]
+        },
+        {
+            fieldName: "UI/UX Design",
+            stack: [
+                {
+                    stackName: "Figma",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "Adobe XD",
+                    level:"beginner"
+                },
+                {
+                    stackName: "Framer",
+                    level:"intermediate"
+                }
+            ]
+        },
+        {
+            fieldName: "Graphic Design",
+            stack: [
+                {
+                    stackName: "Photoshop",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "Illustrator",
+                    level:"beginner"
+                },
+                {
+                    stackName: "Figma",
+                    level:"intermediate"
+                }
+            ]
+        },
+        {
+            fieldName: "Software Development",
+            stack: [
+                {
+                    stackName: "C#",
+                    level:"intermediate"
+                },
+                {
+                    stackName: "Java",
+                    level:"beginner"
+                },
+                {
+                    stackName: ".Net",
+                    level:"intermediate"
+                }
+            ]
+        }
+    ],
+    projects: [
+        {
+            projectName: "VanGo",
+            projectSummary:"An Online Van Reservation App",
+            projectDesc: "academic project made chuchuchu",
+            projectTags: [
+                "Laravel", "Javascript", "HTML", "SASS", "Livewire"
+            ],
+            projectImage: "#",
+            projectLink: "#"
+        },
+        {
+            projectName: "Recipe App",
+            projectSummary:"CRUD",
+            projectDesc: "academic project made chuchuchu",
+            projectTags: [
+                "React", "Tailwind" 
+            ],
+            projectImage: "#",
+            projectLink: "#"
+        },
+    ]
+
+}
