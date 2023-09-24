3f004d30a2bd9c691136855e8f840ea0ce66b1bf
faye <talayfaye@gmail.com>
2023-09-24 16:45:36 +0800

    works + other page updates

--- src/sections/other.jsx
+++ src/sections/other.jsx
@@ -19,9 +19,9 @@ const Other = () => {
   };
 
   return (
-    <div className='px-8 md:px-28 py-16 space-y-8 overflow-hidden'>
+    <div className='px-0 md:px-28 py-16 space-y-8 overflow-hidden'>
 
-      <div className='flex justify-end gap-4'>
+      <div className='flex justify-end gap-4 px-8 md:px-0'>
         <Button toggle={scrollLeft} type="icon" style="outlined" shape="rounded">
           <Icon icon="ion:arrow-back" width="22" className="mx-auto" />
         </Button>
@@ -30,9 +30,9 @@ const Other = () => {
         </Button>
       </div>
 
-      <div className='h-full w-full flex flex-col lg:flex-row gap-x-12'>
+      <div className='h-full w-full flex flex-col lg:flex-row gap-12'>
 
-          <div className='w-full xl:w-1/4'>
+          <div className='w-full xl:w-1/4 px-8 md:px-0'>
 
             <div className="font-secondary text-5xl lg:text-6xl xl:text-7xl font-bold lg:break-words">
                 Other Works
@@ -42,7 +42,7 @@ const Other = () => {
 
           <div className='w-full xl:w-3/4'>
 
-            <div className='flex flex-row flex-nowrap gap-8 overflow-x-auto' ref={sliderRef}>
+            <div className='flex flex-row flex-nowrap gap-2 md:gap-4 lg:gap-8 overflow-x-auto' ref={sliderRef}>
               <div className='h-[36rem] min-w-[26rem] overflow-clip'>
                 <img src='https://images.unsplash.com/photo-1617785899222-fe06b15b6dd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1771&q=80'
                 className='h-full w-full object-cover hover:scale-110 transition-transform duration-500'/>
